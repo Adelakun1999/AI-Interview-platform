a1ab10a9629b2d41f29f74552d589ce40c5e96a0
Adelakun Yusuf Adekunle <kingdomagent1999@gmail.com>
2026-06-01 13:55:46 +0100

    Update BASE_URL to use the production environment URL

--- prepai-frontend/interview-app/src/services/api.js
+++ prepai-frontend/interview-app/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";  
+const BASE_URL = "https://ai-interview-platform-e1nm.onrender.com";
 
 const getToken = () => localStorage.getItem("token");
 
